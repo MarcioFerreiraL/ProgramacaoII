8653ad47c670c067bb9f759c30d97f57780fe5cf
Márcio Ferreira Lima <contactmarcioflima@gmail.com>
2025-04-08 15:13:05 -0300

    .

--- ESAula5/src/exercicio/Calculadora.java
+++ ESAula5/src/exercicio/Calculadora.java
@@ -1,12 +1,90 @@
 package exercicio;
 
+import java.util.List;
+
 public class Calculadora {
 	public static int operacoesRealizadas;
 	
-	public static String multiplicar(double num1, double num2) {
+	public static void multiplicar(double num1, double num2) {
 		operacoesRealizadas++;
-		return String.format("O resultado é: %.2f",num1 * num2);
+		String printar = String.format("O resultado é: %.2f",num1 * num2);
+		System.out.println(printar);
 	}
 	
+	public static void somar(double num1, double num2) {
+		operacoesRealizadas++;
+		String printar = String.format("O resultado é: %.2f",num1 + num2);
+		System.out.println(printar);
+	}
 	
+	public static void subtrair(double num1, double num2) {
+		operacoesRealizadas++;
+		String printar = String.format("O resultado é: %.2f",num1 - num2);
+		System.out.println(printar);
+	}
+	
+	public static void dividir(double num1, double num2) {
+		operacoesRealizadas++;
+		String printar = String.format("O resultado é: %.2f",num1 / num2);
+		System.out.println(printar);
+	}
+	
+	public static String calcularBhaskara(double a, double b, double c) {
+		double delta = (b*b) - 4 * a * c;
+		double x1 = -(b) + Math.sqrt(delta)/(2 * a);
+		double x2 = -(b) - Math.sqrt(delta)/(2 * a);
+		return String.format("O valor de X1 é de: %.2f\nO valor de X2 é de: %.2f", x1, x2);
+	}
+	
+	public static String calcularAreaCirculo(double r) {
+		double resultado = Math.PI * Math.pow(r, 2);
+		return String.format("A área do circulo é: %.2f", resultado);
+	}
+	
+	public static String calcularPitagoras(double a, double b) {
+		double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+		return String.format("O valor da hipotenusa é: %.2f", c);
+	}
+	
+	public static String calcularVolumeEsfera(double r) {
+		double resultado = 4 * (Math.PI * Math.pow(r, 3)) / 3;
+		return String.format("A volume da esfera é: %.2f", resultado);
+	}
+	
+	public static String calcularJurosCompostos(double valor, int ano, double taxa) {
+		double resultado = valor * Math.pow((1 + taxa), ano);
+		return String.format("A montante ao final será de: %.2f", resultado);
+	}
+	
+	public static String calcularAreaTriangulo(double a, double h) {
+		double resultado = a * h / 2;
+		return String.format("A area do tringulo é: %.2f", resultado);
+	}
+	
+	public static String calcularComprimentoCircunferencia(double r) {
+		double resultado = 2 * Math.PI * r;
+		return String.format("O comprimento desta cricunferencia é: %.2f", resultado);
+	}
+	
+	public static String calcularFatorial(int fatorial) {
+		int resultadoFatorial = 1;
+	    for (int i = 1; i <= fatorial; i++) {
+	    	resultadoFatorial *= i;
+	    }
+	    return String.format("O resulto desse fatorial é: %d", resultadoFatorial);
+	}
+	
+	public static String calcularMediaAritmetica(List<Integer> lista) {
+		int valorTotal = 0;
+		for (int numero : lista) {
+			valorTotal += numero;
+		}
+		double resultado = valorTotal / lista.size();
+		return String.format("A media aritmetica é: %.2f", resultado);
+ 	}	
+	
+	public static String calcularVolumeCilindro(double r, double h) {
+		double resultado = Math.PI * Math.pow(r, 2) * h;
+		return String.format("A volume do cilindro é: %.2f", resultado);
+	}
 }
--- ESAula5/src/exercicio/Main.java
+++ ESAula5/src/exercicio/Main.java
@@ -6,25 +6,183 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		double num1, num2, num3;
 		Scanner ler = new Scanner(System.in);
 		List <String> menu = new ArrayList<>();
-		menu.add("-----------------------------");
-		menu.add("            MENU");
-		menu.add("-----------------------------");
-		menu.add("      1 - Adição");
-		menu.add("      2 - Subtração");
-		menu.add("      3 - Multiplicação");
-		menu.add("      4 - Divisão");
-		menu.add("-----------------------------");
+		List <String> menu2 = new ArrayList<>();
+		
+		menu.add("Adição");
+		menu.add("Subtração");
+		menu.add("Multiplicação");
+		menu.add("Divisão");
+		menu.add("Outros Calculos");
+		menu.add("Sair");
+		
+		menu2.add("Formula de Bhaskara");
+		menu2.add("Area do circulo");
+		menu2.add("Pitagoras");
+		menu2.add("Volume da Esfera");
+		menu2.add("Juros Compostos");
+		menu2.add("Area do Triangulo");
+		menu2.add("Comprimento da Circunferencia");
+		menu2.add("Media Aritmetica");
+		menu2.add("Fatorial");
+		menu2.add("Volume do Cilindro");
+		menu2.add("Voltar");
 
-		for (String printar : menu) {
-			System.out.println(printar);
-		}
-		int escolha = ler.nextInt();
-		ler.close();
-		if (escolha == 1) {
+		while (true) {
+			
+			System.out.println("Escolha a Operacao:");
+			
+			// printar o menu
+			for (String printar : menu) {
+				System.out.println((menu.indexOf(printar) + 1) + " - " + printar);
+			}
+			
+			// ler a escolha do usuario
+			int alternativa = ler.nextInt();
 			;
+			
+			if (alternativa == 6) {
+				System.out.println("Até Logo! :)");
+				break;
+			}
+			
+			switch (alternativa) {
+			
+			case 1: // usuario escolhe soma
+				System.out.print("Digite o primeiro numero que voce deseja somar: ");
+				num1 = ler.nextDouble();
+				System.out.print("Digite o segundo numero que voce deseja somar: ");
+				num2 = ler.nextDouble();
+				Calculadora.somar(num1, num2);
+				break;
+			
+			case 2: // usuario escolhe subtracao
+				System.out.print("Digite o primeiro numero que voce deseja subtrair: ");
+				num1 = ler.nextDouble();
+				System.out.print("Digite o segundo numero que voce deseja subtrair: ");
+				num2 = ler.nextDouble();
+				Calculadora.subtrair(num1, num2);
+				break;
+			
+			case 3: // usuario escolhe multiplicacao
+				System.out.print("Digite o primeiro numero que voce deseja multiplicar: ");
+				num1 = ler.nextDouble();
+				System.out.print("Digite o segundo numero que voce deseja multiplicar: ");
+				num2 = ler.nextDouble();
+				Calculadora.multiplicar(num1, num2);
+				break;
+			
+			case 4: // usuario escolhe divisao
+				System.out.print("Digite o primeiro numero que voce deseja dividir: ");
+				num1 = ler.nextDouble();
+				System.out.print("Digite o segundo numero que voce deseja dividir: ");
+				num2 = ler.nextDouble();
+				Calculadora.dividir(num1, num2);
+				break;
+				
+			case 5: {
+				while (true) {
+					for (String printar2 : menu2) {
+						System.out.println((menu2.indexOf(printar2) + 1) + " - " + printar2);
+					}
+					
+					int alternativa2 = ler.nextInt();
+					
+					if (alternativa2 == 11) {
+						System.out.println("Até Logo! :)");
+						break;
+					}
+					
+					switch (alternativa2) {
+						case 1:
+							System.out.print("Digite o primero numero(a) da equacao do segundo grau: ");
+							num1 = ler.nextDouble();
+							System.out.print("Digite o segundo numero(b) da equacao do segundo grau: ");
+							num2 = ler.nextDouble();
+							System.out.print("Digite o terceiro numero(c) da equacao do segundo grau: ");
+							num3 = ler.nextDouble();
+							String resultadoBhaskara = Calculadora.calcularBhaskara(num1, num2, num3);
+							System.out.println(resultadoBhaskara);
+							break;
+						case 2:
+							System.out.print("Digite o raio do circulo: ");
+							num1 = ler.nextDouble();
+							String resultadoAreaCirculo = Calculadora.calcularAreaCirculo(num1);
+							System.out.println(resultadoAreaCirculo);
+							break;
+						case 3: 
+							System.out.println("Digite o valor de um cateto: ");
+							num1 = ler.nextDouble();
+							System.out.println("Digite o valor de outro cateto: ");
+							num2 = ler.nextDouble();
+							String resultadoPitagoras = Calculadora.calcularPitagoras(num1, num2);
+							System.out.println(resultadoPitagoras);
+						case 4:
+							System.out.print("Digite o raio da esfera: ");
+							num1 = ler.nextDouble();
+							String resultadoVolumeEsfera = Calculadora.calcularVolumeEsfera(num1);
+							System.out.println(resultadoVolumeEsfera);
+							break;
+						case 5:
+							System.out.print("Digite o capital inicial a ser investido: ");
+							num1 = ler.nextDouble();
+							System.out.print("Digite o tempos em anos que o capital ficará aplicado: ");
+							int ano = ler.nextInt();
+							System.out.print("Digite a taxa de juros que será aplicada sobre o capital: ");
+							num3 = ler.nextDouble();
+							String resultadoJurosCompostos = Calculadora.calcularJurosCompostos(num1, ano, num3);
+							System.out.println(resultadoJurosCompostos);
+							break;
+						case 6:
+							System.out.println("Digite o valor da base do tringulo: ");
+							num1 = ler.nextDouble();
+							System.out.println("Digite o valor da altura do tringulo:");
+							num2 = ler.nextDouble();
+							String resultadoAreaTriangulo = Calculadora.calcularAreaTriangulo(num1, num2);
+							System.out.println(resultadoAreaTriangulo);
+							break;
+						case 7: 
+							System.out.print("Digite o raio da circunferencia: ");
+							num1 = ler.nextDouble();
+							String resultadoComprimentoCircunferencia = Calculadora.calcularComprimentoCircunferencia(num1);
+							System.out.println(resultadoComprimentoCircunferencia);
+							break;
+						case 8:
+							List <Integer> numeros = new ArrayList<>();
+							while (true) {
+								System.out.println("Digite um numero para adicionar a media aritmetica (digite 0 para parar)");
+								int numero = ler.nextInt();
+								if (numero == 0) {
+									break;
+								}
+								numeros.add(numero);
+								String resultadoMediaAritmetica = Calculadora.calcularMediaAritmetica(numeros);	
+								System.out.println(resultadoMediaAritmetica);
+								break;
+							}
+						case 9:
+							System.out.print("Digite o numero para calcular o faotrial do mesmo: ");
+							int numero = ler.nextInt();
+							String resultadoCalcularFatorial = Calculadora.calcularFatorial(numero);
+							System.out.println(resultadoCalcularFatorial);
+							break;
+						case 10: 
+							System.out.print("Digite o raio da cilindro: ");
+							num1 = ler.nextDouble();
+							System.out.print("Digite a altura do cilindro: ");
+							num2 = ler.nextDouble();
+							String resultadoVolumeCilindro = Calculadora.calcularVolumeCilindro(num1, num2);
+							System.out.println(resultadoVolumeCilindro);
+							break;
+						}	
+					
+				}
+			ler.close();
 		}
-	
-	}
+			}		
+}
+}
 }
